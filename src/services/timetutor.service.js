6f93d40fe58fba7c37d01c3a2857544623bb9276
junghakim2023 <jungha.kim.2023@gmail.com>
2023-11-24 16:40:04 -0800

    Feature : backend - connect to managerpage, schedulepage

--- src/services/timetutor.service.js
+++ src/services/timetutor.service.js
@@ -10,6 +10,14 @@ const mainPage = function(req, res){
     res.sendFile(path.resolve('res/mainPage.html'));
 }
 
+const schedularPage = function(req, res){
+    res.sendFile(path.resolve('res/schedularPage.html'));
+}
+
+const managerPage = function(req, res){
+    res.sendFile(path.resolve('res/managerPage.html'));
+}
+
 function setCommonData(data){
     data.loginURL = process.env.LOGIN_URL + "/login";
     return data;
@@ -78,4 +86,4 @@ function resolveJwtToken(token){
     
   }
 
-module.exports = {testPage, mainPage, init, checkLogin}
+module.exports = {testPage, mainPage, managerPage, schedularPage, init, checkLogin}
